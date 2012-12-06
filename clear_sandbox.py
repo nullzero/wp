87e4f56d61b2d5406639c2f6c138101ee7ee7a62
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2012-12-07 01:13:23 +0700

    add script for clearing sandbox

---
+++ clear_sandbox.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import wikipedia as pywikibot
+
+pywikibot.handleArgs(u"-log")
+vlist = []
+vlist.append(u"วิกิพีเดีย:สอนการใช้งาน_(จัดรูปแบบ)/กระดาษทด")
+vlist.append(u"วิกิพีเดีย:สอนการใช้งาน_(แหล่งข้อมูลอื่น)/กระดาษทด")
+vlist.append(u"วิกิพีเดีย:สอนการใช้งาน_(แก้ไข)/กระดาษทด")
+vlist.append(u"วิกิพีเดีย:สอนการใช้งาน_(วิกิพีเดียลิงก์)/กระดาษทด")
+vlist.append(u"วิกิพีเดีย:ทดลองเขียน")
+
+site = pywikibot.getSite()
+text = u"{{ทดลองเขียน}}<!-- กรุณาอย่าแก้ไขบรรทัดนี้ ขอบคุณครับ/ค่ะ -- Please leave this line as they are. Thank you! -->\n"
+
+for i in vlist:
+    page = pywikibot.Page(site, i)
+    page.put(text, u"ล้างหน้าอัตโนมัติด้วยบอต")
+
+pywikibot.stopme()
