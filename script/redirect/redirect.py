340c78f6d7db13fbc4fe7d951e34dfbb47bb917e
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2012-12-12 01:36:26 +0700

    automate logging

--- script/redirect/redirect.py
+++ script/redirect/redirect.py
@@ -6,6 +6,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 from pywikipedia.redirect import RedirectGenerator, RedirectRobot
 
 if __name__ == "__main__":
+    pywikibot.handleArgs("-log")
+    pywikibot.output(u"'redirect script' is invoked. (%s)" % preload.getTime())
     gen = RedirectGenerator(namespaces = [0], use_api = True)
     bot = RedirectRobot(action = 'both', generator = gen, always = True)
     bot.run()
+    pywikibot.output(u"'redirect script' terminated. (%s)" % preload.getTime())
+    pywikibot.stopme()
